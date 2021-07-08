4c75d6bc90c5d471097317bd35e961edce209865
angusb93 <angusbuick@gmail.com>
2021-07-08 15:45:10 +1000

    modified port settings

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
 });
